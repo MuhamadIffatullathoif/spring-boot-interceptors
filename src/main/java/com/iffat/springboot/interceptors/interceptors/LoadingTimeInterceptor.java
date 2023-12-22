9766c146acaf092bec218946eddbb6cd6330edb2
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 15:41:34 +0700

    02. Registering interceptor in spring configuration

--- src/main/java/com/iffat/springboot/interceptors/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/iffat/springboot/interceptors/interceptors/LoadingTimeInterceptor.java
@@ -2,18 +2,26 @@ package com.iffat.springboot.interceptors.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("Loading time interceptor preHandle()");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("Loading time interceptor postHandle()");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 }
