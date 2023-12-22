d2138789f24e815b9a3d162c4af69f38ad1d61e7
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 15:27:18 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/interceptors/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,19 @@
+package com.iffat.springboot.interceptors.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoadingTimeInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
