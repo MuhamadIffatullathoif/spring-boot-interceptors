2ef8a458fa82b7f746b76dd8d156e1d39666729b
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 15:59:34 +0700

    03. Implementing ElapsedTime Interceptor

--- src/main/java/com/iffat/springboot/interceptors/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/iffat/springboot/interceptors/interceptors/LoadingTimeInterceptor.java
@@ -9,6 +9,8 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Random;
+
 @Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
 
@@ -16,13 +18,27 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
         HandlerMethod method = (HandlerMethod) handler;
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
+
         logger.info("Loading time interceptor preHandle() " + method.getMethod().getName());
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        long end = System.currentTimeMillis();
+        long start = (long) request.getAttribute("start");
+        long result = end - start;
+
+        logger.info("ElapsedTime: {} millisecond", result);
         logger.info("Loading time interceptor postHandle() "+ ((HandlerMethod) handler).getMethod().getName());
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
